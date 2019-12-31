70a5bd7b5fe4d898497da4baa11799b8c41bfe7a
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:34:01 -0600

    Rewriting as Laminas Project package

--- test/TestAsset/Page.php
+++ test/TestAsset/Page.php
@@ -1,20 +1,18 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Navigation
+ * @see       https://github.com/laminas/laminas-navigation for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-navigation/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-navigation/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Navigation\TestAsset;
+namespace LaminasTest\Navigation\TestAsset;
 
-use Zend\Navigation\Page\AbstractPage;
+use Laminas\Navigation\Page\AbstractPage;
 
 /**
- * @category   Zend
- * @package    Zend_Navigation
+ * @category   Laminas
+ * @package    Laminas_Navigation
  * @subpackage UnitTests
  */
 class Page extends AbstractPage
