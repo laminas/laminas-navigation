1b44f39fd2f6749f16e84944e16ac3d05d756194
Andi Rückauer <arueckauer@gmail.com>
2021-09-30 10:07:59 +0200

    Remove file headers
    Signed-off-by: Andi Rückauer <arueckauer@gmail.com>

--- src/View/ViewHelperManagerDelegatorFactory.php
+++ src/View/ViewHelperManagerDelegatorFactory.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-navigation for the canonical source repository
- * @copyright https://github.com/laminas/laminas-navigation/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-navigation/blob/master/LICENSE.md New BSD License
- */
-
 namespace Laminas\Navigation\View;
 
 use Interop\Container\ContainerInterface;
